db6d6cefc7aba0b6184efbda19c3bcb9572fe689
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-07-03 15:16:41 +0100

    Deleted dublin bus folder, moving in the right place

--- DublinBus/TTM/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Routes, Stops
-
-admin.site.register(Routes)
-admin.site.register(Stops)
--- DublinBus/TTM/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TtmConfig(AppConfig):
-    name = 'TTM'
