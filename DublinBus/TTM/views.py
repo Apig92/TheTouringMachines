ef58652e28ddceac74421458785c7ed8cf903d81
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-07-24 14:16:06 +0100

    Fixed url names (case-sensitive browser)

--- DublinBus/TTM/views.py
+++ DublinBus/TTM/views.py
@@ -7,25 +7,25 @@ import json
 def index(request):
     all_routes = Routes.objects.all()
     context = {'all_routes': all_routes,}
-    return render(request, 'ttm/index.html', context)
+    return render(request, 'TTM/index.html', context)
 
 def detail(request, Route_ID):
     try:
         route = Routes.objects.get(pk=Route_ID)
     except Routes.DoesNotExist:
         raise Http404("Route does not exist")
-    return render(request, 'ttm/detail.html', {'route': route})
+    return render(request, 'TTM/detail.html', {'route': route})
 
 def json_routes(request):
     #json_data = json.dumps('TTM/static/TTM/JSON/routes.json')
-    return render(request, 'ttm/json_routes.html')
+    return render(request, 'TTM/json_routes.html')
 
 def json_jpid(request, jpid):
     #json_jpid = json.dumps('TTM/static/TTM/JSON/' + jpid + '.json')
-    return render(request, 'ttm/json_jpid.html')
+    return render(request, 'TTM/json_jpid.html')
 
 def map(request):
-    return render(request, 'ttm/map.html')
+    return render(request, 'TTM/map.html')
 
 def icon_circle(request):
     return render(request)
