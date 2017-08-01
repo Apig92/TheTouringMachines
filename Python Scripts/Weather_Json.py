204859ffef450133f751b29dfd958d873205a3e8
Dcummins1 <daniel.cummins00@gmail.com>
2017-08-01 11:22:31 +0100

    weather scraper - need to run every 3 hrs, will edit to do so

---
+++ Python Scripts/Weather_Json.py
@@ -0,0 +1,18 @@
+import urllib.request
+import json
+
+api_request = "http://api.openweathermap.org/data/2.5/forecast?id=2964574&APPID=e9da13ccf40ebb756a8680b64650d626"
+#this can only be called every 10 mins or will get blocked, so will call every 3 hours and save as json
+
+def getjson(api_request):
+    url = urllib.request.urlopen(api_request)
+    print ('request made')
+    output = url.read().decode('utf-8')
+    weatherjson = json.loads(output)
+    url.close()
+    with open('C:/Users/Daniel/PycharmProjects/TheTouringMachines/DublinBus/TTM/static/TTM/JSON/weather.json', 'w') as outfile:
+        json.dump(weatherjson, outfile)
+
+getjson(api_request)
+
+
