c0b8fd828cfb448ee815e89c15c03350772f01ff
Dcummins1 <daniel.cummins00@gmail.com>
2017-08-03 16:06:34 +0100

    Merge branch 'master' of C:\Users\Daniel\PycharmProjects\TheTouringMachines with conflicts.

--- DublinBus/DublinBus/settings.py
+++ DublinBus/DublinBus/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'DublinBus.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dublinbus',
-        'USER': 'andy',
-        'PASSWORD': 'hotchocolate',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    # 'default': {
+    #     #'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'DublinBus',
+    #     'USER': 'root',
+    #     'PASSWORD': 'wertyu',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
