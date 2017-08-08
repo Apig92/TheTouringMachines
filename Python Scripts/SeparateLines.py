170c1e04fac45f0c98160c04974a291cdb1d4f5a
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-08-08 11:35:36 +0100

    Updated line script

--- Python Scripts/SeparateLines.py
+++ Python Scripts/SeparateLines.py
@@ -1,4 +1,4 @@
-#run script in the server (home directory), and obtain a csv for each line
+#run script in the server, and obtain a csv for each line
 
 import pandas as pd
 import csv
@@ -6,47 +6,144 @@ import numpy as np
 import glob
 import time
 import numpy
+import datetime
+import os
+
+
+def getRoutes(path):
+    routes= set()
+    for filename in os.listdir(path):
+        if filename.endswith(".csv"):
+            x = "" + filename + ""
+            df = pd.read_csv(x, index_col=None, header=0, low_memory=False)
+            df.columns = ["Timestamp", "LineId", "Direction", "JourneyPattern", "Timeframe", "VehicleJourney",
+                          "Operator",
+                          "Congestion", "Longitude", "Latitude", "Delay", "BlockId", "VehicleId", "StopId", "AtStop"]
+            print("Working on day", x)
+            #df1 = df.LineId.apply(str)
+            # df['LineId'] = df['LineId'].astype(str)
+
+            lines = df['LineId'].unique()
+            lines1 = lines[~pd.isnull(lines)]
+            routes = routes.union(set(lines1))
+
+
+        else:
+            print(filename, "is not a.csv")
+
+    routes=list(routes)
+
+    return routes
+
+
+def separateLines(path,routes):
+    path2 = "/home/csstudent/AllLines"  # CHANGE ME
+    for line in routes:
+        frame = pd.DataFrame()
+        list_ = []
+        print("Creating line:", line)
+        for file in os.listdir(path):
+            if file.endswith(".csv"):
+                x = "" + file + ""
+                df = pd.read_csv(x, index_col=None, header=0, low_memory=False)
+                df.columns = ["Timestamp", "LineId", "Direction", "JourneyPattern", "Timeframe", "VehicleJourney",
+                              "Operator",
+                              "Congestion", "Longitude", "Latitude", "Delay", "BlockId", "VehicleId", "StopId",
+                              "AtStop"]
+                df['LineId'] = df['LineId'].astype(str)
+                df1 = df[df['LineId'] == line]
+                list_.append(df1)
+            else:
+                print(file, "is not a.csv")
+        frame = pd.concat(list_)
+        frame = frame.reset_index(drop=True)
+        frame['Date'] = pd.to_datetime(frame['Timestamp'], unit='us')
+        # get rid of the night busses
+        daytime = frame[(frame.Date.dt.hour != 1) & (frame.Date.dt.hour != 2) & (frame.Date.dt.hour != 3) & (
+            frame.Date.dt.hour != 4) & (frame.Date.dt.hour != 5)]
+        print('Working on: ' + 'line' + str(line) + 'CLEAN.csv')
+        daytime.to_csv(path2 + '/line' + str(line) + 'CLEAN.csv', index=False)
+        print('Finished: ' + 'line' + str(line) + 'CLEAN.csv')
+
+
+
+def main():
+    path = '/home/csstudent/AllDays'  # CHANGE ME
+    routes= getRoutes(path)
+    print(routes)
+    #separateLines(path,routes)
+
+
+#run script in the server (home directory), and obtain a csv for each line
+
 
 
 # This is the path where the CSVs are: do not put anything else in here, as the program loads all the content.
-path = r'./AllDays'  # CHANGE ME
+path = '/home/csstudent/AllDays'  # CHANGE ME
 
 # This is the path where you want to save the new CSVs: change it to whatever you want
-path2 = "./AllLines"  # CHANGE ME
-allFiles = glob.glob("/*.csv")
+path2 = "/home/csstudent/AllLines"  # CHANGE ME
+allFiles = glob.glob(path+"/*.csv")
 
-# get lines by reading one of the CSVs
-df = pd.read_csv(path+ "/siri.20121106.csv", index_col=None, header=0, low_memory=False)  # Use whatever file you wish
-df.columns = ["Timestamp", "LineId", "Direction", "JourneyPattern", "Timeframe", "VehicleJourney", "Operator",
-              "Congestion", "Longitude", "Latitude", "Delay", "BlockId", "VehicleId", "StopId", "AtStop"]
+#routes = getRoutes(path)
 
-# Save the lines in an array
-lines = df['LineId'].unique()
-lines = lines[~pd.isnull(lines)]
+routes= set()
+for filename in os.listdir(path):
+    if filename.endswith(".csv"):
+        x = "" + filename + ""
+        df = pd.read_csv(x, index_col=None, header=0, low_memory=False)
+        df.columns=["Timestamp","LineId","Direction","JourneyPattern","Timeframe","VehicleJourney","Operator","Congestion","Longitude","Latitude","Delay","BlockId","VehicleId","StopId","AtStop"]
 
-print(lines)
+        #print("Working on day", x)
+            #df1 = df.LineId.apply(str)
+        #df['LineId'] = df['LineId'].astype(str)
+        df['LineId'] = df['LineId'].astype('str')
+
+        lines = df['LineId'].unique()
+        lines1 = lines[~pd.isnull(lines)]
+        routes = routes.union(set(lines1))
+    else:
+        continue
+
+routes=list(routes)
+print(routes)
+
+
 # This creates a new CSV for each route
 
-for line in lines:
-    frame = pd.DataFrame()
-    list_ = []
-    print("Creating line:", line)
-    for file_ in allFiles:
-        df = pd.read_csv(file_, index_col=None, header=0, low_memory=False)
-        df.columns = ["Timestamp", "LineId", "Direction", "JourneyPattern", "Timeframe", "VehicleJourney", "Operator",
-                      "Congestion", "Longitude", "Latitude", "Delay", "BlockId", "VehicleId", "StopId", "AtStop"]
-        df= df.LineId.apply(str)
-        df1 = df[df['LineId'] == line]
-        list_.append(df1)
-    frame = pd.concat(list_)
-    frame = frame.reset_index(drop=True)
-    frame['Date'] = pd.to_datetime(frame['Timestamp'], unit='us')
-    #get rid of the night busses
-    daytime = frame[(frame.Date.dt.hour != 1) & (frame.Date.dt.hour != 2) & (frame.Date.dt.hour != 3) & (
-    frame.Date.dt.hour != 4) & (frame.Date.dt.hour != 5)]
-    print('Working on: ' + 'line' + str(line) + 'CLEAN.csv')
-    daytime.to_csv(path2 + '/line' + str(line) + 'CLEAN.csv', index=False)
-    print('Finished: ' + 'line' + str(line) + 'CLEAN.csv')
+for line in routes:
+    if not os.path.exists(path2 + '/line' + str(line) + 'CLEAN.csv'):
+        frame = pd.DataFrame()
+        list_ = []
+        print("Creating line:", line)
+        for file_ in allFiles:
+            df = pd.read_csv(file_, index_col=None, header=0, low_memory=False)
+            print("working on", file_)
+            df.columns=["Timestamp","LineId","Direction","JourneyPattern","Timeframe","VehicleJourney","Operator","Congestion","Longitude","Latitude","Delay","BlockId","VehicleId","StopId","AtStop"]
+            #df= df.LineId.apply(str)
+            df['LineId'] = df['LineId'].astype('str')
+
+            df1 = df[df['LineId'] == line]
+            list_.append(df1)
+        frame = pd.concat(list_)
+        frame = frame.reset_index(drop=True)
+        frame['Date'] = pd.to_datetime(frame['Timestamp'], unit='us')
+        #get rid of the night busses
+        daytime = frame[(frame.Date.dt.hour != 1) & (frame.Date.dt.hour != 2) & (frame.Date.dt.hour != 3) & (
+        frame.Date.dt.hour != 4) & (frame.Date.dt.hour != 5)]
+        print('Working on: ' + 'line' + str(line) + 'CLEAN.csv')
+        daytime.to_csv(path2 + '/line' + str(line) + 'CLEAN.csv', index=False)
+        print('Finished: ' + 'line' + str(line) + 'CLEAN.csv')
+    else:
+        print(line,'alredy exists')
+
+
+
+
+
+
+
+
 
 
 
