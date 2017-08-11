bce60efb8e58e7a670c7bd9fb5d2b02c6f0639b6
ShanmingLi <shanming.li@ucdconnect.ie>
2017-08-11 12:59:04 +0100

    changed url pattern

--- DublinBus/TTM/urls.py
+++ DublinBus/TTM/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^est_time.html$', views.timepredict, name='est_time'),
 
     # static/TTM/pickles/line.sav
-    url(r'^static/TTM/pickles/[\d]*[\w]*.sav$', views.pickle, name='pickle'),
+    url(r'^static/TTM/pickles/[\d]*[\w]*CLEANrouteweather.sav$', views.pickle, name='pickle'),
 
     # /TTM/JSON/weather.json
     url(r'^static/TTM/JSON/weather.json$', views.weather, name='weather'),
